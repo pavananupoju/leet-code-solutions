72b56e763707b8ecefbd50b0334215471a244136
PAVAN <160342983+pavananupoju@users.noreply.github.com>
2026-01-21 18:19:01 +0530

    Time: 0 ms (100%), Space: 45 MB (6.89%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -10,7 +10,7 @@ class Solution {
             {
                 return mid;
             } 
-            else if(nums[mid]>=target) 
+            else if(nums[mid]>target) 
             {
                 high =  mid-1 ;
             }
