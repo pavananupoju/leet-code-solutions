a3e170523108ace2093208ad5da3b7a1aabf3585
PAVAN <160342983+pavananupoju@users.noreply.github.com>
2026-06-15 19:02:44 +0530

    Time: 1676 ms (8.55%), Space: 43.5 MB (79.86%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,41 @@
 class Solution {
-    int max = 0;
-    int st = 0;
-    int en = 0;
+    public String longestPalindrome(String s) {   
 
-    public String longestPalindrome(String s) {
-
-        for(int i = 0; i < s.length(); i++) {
-            for(int j = i; j < s.length(); j++) {
-
-                if(ispalindrome(s, i, j)) {
-                    if((j - i + 1) > max) {
-                        max = j - i + 1;
-                        st = i;
+        int max =0 ;
+        int st =0 ;
+        int en =0 ;
+        for(int i=0;i<s.length();i++) 
+        {
+            for(int j=i;j<s.length();j++) 
+            {
+                if(isPalin(s, i,j)) 
+                {
+                    if((j-i+1)>max) 
+                    {
+                        max = j-i+1;
+                        st =i ;
                         en = j;
                     }
                 }
             }
         }
-        return s.substring(st, en + 1);
+        return s.substring(st,en+1);
+
+        
     }
 
-    public boolean ispalindrome(String a, int i, int j) {
-
-        while(i < j) {
-            if(a.charAt(i) != a.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+    public boolean isPalin(String s , int i , int j) 
+    {
+             while(i<j) 
+              
+              {
+                if(s.charAt(i)!=s.charAt(j)) 
+                {
+                    return false;
+                }
+                i++;
+                j--;
+              }
+              return true;
     }
 }
