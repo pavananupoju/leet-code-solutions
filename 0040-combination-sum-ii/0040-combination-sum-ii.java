7e7994a936cc4109911cfbcacd0cc7ff13718793
PAVAN <160342983+pavananupoju@users.noreply.github.com>
2026-09-02 21:45:18 +0530

    Time: 7 ms (36.42%), Space: 44.9 MB (93.74%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -25,7 +25,21 @@ class Solution {
        
 
       for(int i=index;i<candidates.length;i++) 
-      {
+      {  
+
+        /* we are cheking the elements at each level we are picking the elements and checking the duplicate occurence if it is dulicate we need to skip that element 
+
+        that why we are chekinf i>index -->index is level and i is the current element
+        index is 0
+        then i loop from --> 0 1 2 3 4 5 6
+        0>0 falwe false --> pick 
+        i>0 next element i>0 ture we need to i-1 == i
+        2>0
+        
+        
+        */
+
+
         if(i>index && candidates[i]==candidates[i-1]) 
         {
             continue;
