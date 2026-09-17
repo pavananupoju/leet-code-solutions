f621edb96dc228cff9d8bbfadb2c561731ce42ed
PAVAN <160342983+pavananupoju@users.noreply.github.com>
2026-09-17 14:44:33 +0530

    Time: 2 ms (99.96%), Space: 49.2 MB (22.61%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -3,23 +3,20 @@ class Solution {
        
 
        int cnt=0;
+       int st =0 ;
+       int pro =1;
+          if (k <= 1) {
+            return 0;
+        }
         for(int i=0;i<nums.length;i++) 
-        {  int pro=1;
-       
-            for(int j=i;j<nums.length;j++) 
-            {
-                pro = pro*nums[j];
-                if(pro<k) 
-                { 
-                    cnt++;
-
-                }
-                else 
-                {
-                    break;
-                }
-            }
-  
+        {  
+            pro*=nums[i];
+           while(pro>=k) 
+           {
+            pro= pro/nums[st];
+            st++;
+           }
+           cnt =  cnt+ (i - st +1);
         }
         return cnt;
         
